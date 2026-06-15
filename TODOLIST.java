be2c720fbcaee8ca74b66f3b8d79e712facb7442
Joshua Bongcayao <Joshuaesin270@gmail.com>
2026-06-16 01:43:54 +0800

    added edit

--- TODOLIST.java
+++ TODOLIST.java
@@ -9,14 +9,15 @@ public class TODOLIST {
             System.out.println("[1] Add Task");
             System.out.println("[2] View Tasks");
             System.out.println("[3] Remove Task");
-            System.out.println("[4] Exit");
+            System.out.println("[4] Edit Task");
+            System.out.println("[5] Exit");
 
             System.out.println("Enter an option: ");
             int option = -1;
 
             try {
                 option = Integer.parseInt(scanner.nextLine());
-                if(option < 1 || option >= 5) {
+                if(option < 1 || option >= 6) {
                     System.out.println("Out of range!");
                     continue;
                 }
@@ -29,7 +30,8 @@ public class TODOLIST {
                 case 1 -> ADD();
                 case 2 -> VIEW();
                 case 3 -> REMOVE();
-                case 4 -> {
+                case 4 -> EDIT();
+                case 5 -> {
                     while(true) {
                         System.out.println("Do you want to exit? (Yes/No)");
                         String action = scanner.nextLine();
@@ -105,6 +107,41 @@ public class TODOLIST {
             else {
                 String removed = array.remove(option - 1);
                 System.out.println("Successfully removed: " + removed);
+                return;
+            }
+        }
+    }
+    static void EDIT() {
+        while(true) {
+            System.out.println("=====EDIT-MENU=====");
+
+            if(array.isEmpty()) {
+                System.out.println("Nothing to edit.");
+                return;
+            }
+
+            for(int i = 0; i < array.size(); i++) {
+                System.out.println((i + 1) + "." + array.get(i));
+            }
+
+            System.out.println("Enter a task number to edit: ");
+            int choice;
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch(NumberFormatException e) {
+                System.out.println("Numbers only!");
+                continue;
+            }
+            if(choice > array.size()) {
+                    System.out.println("Out of range!");
+                    continue;
+            } else {
+                System.out.println("Enter a new task name: ");
+                String newName = scanner.nextLine();
+
+                array.set(choice -1, newName);
+        
+                System.out.println("Successfully changed to: " + newName);
                 return;
             }
         }
